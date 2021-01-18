a9f54a558c854ed9b0fa529d53d721f8677084d3
Pavlo Chabanenko <chabanenkopp@gmail.com>
2021-01-18 21:40:47 +0100

    Remove unnecessary consts

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,3 @@
-import { COLOR } from 'Theme'
-
-export const SCROLL_TO_NAME = {
-  USP: 'usp',
-  SUBSCRIBE: 'subscribe',
-}
-
-export const EMAIL = {}
-
-export const URL = {}
-
-export const AOS_ANIMATION = { FADE_UP: { 'data-aos': 'fade-up' } }
-
 export const GOOGLE_MAP_URL = `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GATSBY_GOOGLE_KEY}`
 
 export const MAP_SETTINGS = {
@@ -35,32 +22,6 @@ export const MAP_SETTINGS = {
     LINE: {
       X: 0,
       Y: 20,
-    },
-  },
-  POLYLINE_OPTIONS: {
-    DASHED: {
-      geodesic: true,
-      strokeOpacity: 0,
-      strokeWeight: 2,
-      strokeColor: COLOR.BLUEBERRY_SODA,
-      icons: [
-        {
-          icon: {
-            path: 'M 0,0 0,1',
-            strokeOpacity: 1,
-            strokeWeight: 2,
-            scale: 3,
-          },
-          offset: '0',
-          repeat: '10px',
-        },
-      ],
-    },
-    REGULAR: {
-      geodesic: true,
-      strokeOpacity: 1,
-      strokeWeight: 2,
-      strokeColor: COLOR.EXPLORATION_GREEN,
     },
   },
   DIRECTIONS_OPTIONS: { suppressMarkers: true, preserveViewport: true },

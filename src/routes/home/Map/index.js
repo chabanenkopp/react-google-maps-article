027f21ab6761d5d789ba08d3d9fd901e88cfd5ec
Pavlo Chabanenko <chabanenkopp@gmail.com>
2021-01-19 03:14:44 +0100

    Polish code

--- src/routes/home/Map/index.js
+++ src/routes/home/Map/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { RADIUS } from 'Theme'
 import { GOOGLE_MAP_URL } from 'constants/constants'
 import { Box } from 'components/Layout'
 import MapContainer from './Map'
@@ -14,13 +13,8 @@ const StyledBox = styled(Box)`
   }
   .gm-style-iw.gm-style-iw-c {
     padding: 0;
-    max-width: none !important;
-    max-height: none !important;
-    border-radius: ${RADIUS.M};
     .gm-style-iw-d {
       overflow: hidden !important;
-      max-width: none !important;
-      max-height: none !important;
     }
   }
   .gmnoprint.gm-bundled-control.gm-bundled-control-on-bottom {
@@ -31,7 +25,7 @@ const StyledBox = styled(Box)`
   height: 100vh;
 `
 
-const Map = ({ ...props }) => (
+const Map = (props) => (
   <StyledBox>
     <MapContainer
       googleMapURL={GOOGLE_MAP_URL}

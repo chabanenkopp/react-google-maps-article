d4c4180387747b4743c75b69700a191b021a94b0
Pavlo Chabanenko <chabanenkopp@gmail.com>
2021-02-05 03:14:43 +0100

    Fix eslint, remove title

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -5,10 +5,7 @@ import ListMapSection from './ListMapSection'
 
 const Home = () => (
   <GenericPage>
-    <SEO
-      title=""
-      description="Nauč sa nové zručnosti v našej IT akadémii v Košiciach a získaj možnosť pracovného povovoru u niektorého z našich IT partnerov."
-    />
+    <SEO title="" description="" />
     <ListMapSection />
   </GenericPage>
 )
